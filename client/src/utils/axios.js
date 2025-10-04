48bc658edbe8eff4d7360594e4bd7c689387e363
ajayarumugam73@gmail.com <you@example.com>
2025-10-04 11:15:18 +0530

    backend url updated

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://xavier-s-ide-server.vercel.app/api",
 });
 
 instance.interceptors.request.use((config) => {
